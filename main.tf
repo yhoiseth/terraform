e45946df807d452e2ad9f53cad9654cd86384ec0
Yngve Høiseth <yhoiseth@gmail.com>
2017-09-13 16:24:29 +0200

    Add simple webpage

--- main.tf
+++ main.tf
@@ -6,8 +6,26 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   tags {
     Name = "terraform-example"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo 'Hello, World!' > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
