e84284a6dd2e5b668fa36366117bbea99882c040
Yngve Høiseth <yngve@hoiseth.net>
2017-09-16 18:46:19 +0200

    Add global S3

---
+++ global/s3/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "s3" {
+    bucket = "yhoiseth-terraform-example-state"
+    key = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  version = "~> 0.1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "yhoiseth-terraform-example-state"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
