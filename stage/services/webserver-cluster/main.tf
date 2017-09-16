004f3ae7687f5391621060fd184b7a51b407c593
Yngve Høiseth <yngve@hoiseth.net>
2017-09-16 19:05:40 +0200

    Refactor

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "yhoiseth-terraform-example-state"
+    key = "stage/services/webserver-cluster/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
   version = "~> 0.1"
